3891c14d3ae1a34201987ed367de9af5dc3cc28e
Cledson Henrique <cledson_henrique@hotmail.com>
2022-05-27 22:00:59 -0300

    Exercícos MySQL

---
+++ Aula 16 Exercicio ListaRep.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE ListaRep;
+
+USE ListaRep;
+
+CREATE TABLE artists (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nome TEXT,
+    country TEXT,
+    genre TEXT);
+
+INSERT INTO artists (nome, country, genre)
+    VALUES ("Taylor Swift", "US", "Pop");
+INSERT INTO artists (nome, country, genre)
+    VALUES ("Led Zeppelin", "US", "Hard rock");
+INSERT INTO artists (nome, country, genre)
+    VALUES ("ABBA", "Sweden", "Disco");
+INSERT INTO artists (nome, country, genre)
+    VALUES ("Queen", "UK", "Rock");
+INSERT INTO artists (nome, country, genre)
+    VALUES ("Celine Dion", "Canada", "Pop");
+INSERT INTO artists (nome, country, genre)
+    VALUES ("Meatloaf", "US", "Hard rock");
+INSERT INTO artists (nome, country, genre)
+    VALUES ("Garth Brooks", "US", "Country");
+INSERT INTO artists (nome, country, genre)
+    VALUES ("Shania Twain", "Canada", "Country");
+INSERT INTO artists (nome, country, genre)
+    VALUES ("Rihanna", "US", "Pop");
+INSERT INTO artists (nome, country, genre)
+    VALUES ("Guns N' Roses", "US", "Hard rock");
+INSERT INTO artists (nome, country, genre)
+    VALUES ("Gloria Estefan", "US", "Pop");
+INSERT INTO artists (nome, country, genre)
+    VALUES ("Bob Marley", "Jamaica", "Reggae");
+
+CREATE TABLE songs (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    artist TEXT,
+    title TEXT);
+
+INSERT INTO songs (artist, title) VALUES ("Taylor Swift", "Shake it off");
+INSERT INTO songs (artist, title) VALUES ("Rihanna", "Stay");
+INSERT INTO songs (artist, title) VALUES ("Celine Dion", "My heart will go on");
+INSERT INTO songs (artist, title) VALUES ("Celine Dion", "A new day has come");
+INSERT INTO songs (artist, title) VALUES ("Shania Twain", "Party for two");
+INSERT INTO songs (artist, title) VALUES ("Gloria Estefan", "Conga");
+INSERT INTO songs (artist, title) VALUES ("Led Zeppelin", "Stairway to heaven");
+INSERT INTO songs (artist, title) VALUES ("ABBA", "Mamma mia");
+INSERT INTO songs (artist, title) VALUES ("Queen", "Bicycle Race");
+INSERT INTO songs (artist, title) VALUES ("Queen", "Bohemian Rhapsody");
+INSERT INTO songs (artist, title) VALUES ("Guns N' Roses", "Don't cry");
+
+SELECT title FROM songs WHERE artist = ("Queen"); #Etapa 1
+
+SELECT nome FROM artists WHERE genre = ("Pop") #Etapa 2
+
